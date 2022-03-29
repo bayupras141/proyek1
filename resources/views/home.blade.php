5bcbae65976cee47b8c429080b38b4ffa295af45
Arfiyan <arfieyan1903@gmail.com>
2022-03-29 09:53:04 +0700

    coba-commit-pian

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
           <div class="inner">
             <h3>150</h3>
 
-            <p>New Orders</p>
+            <p>Kendaraan</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
@@ -38,9 +38,9 @@
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>53<sup style="font-size: 20px">%</sup></h3>
+          <h3>150</h3>
 
-            <p>Bounce Rate</p>
+            <p>Customer</p>
           </div>
           <div class="icon">
             <i class="ion ion-stats-bars"></i>
@@ -55,7 +55,7 @@
           <div class="inner">
             <h3>44</h3>
 
-            <p>User Registrations</p>
+            <p>User</p>
           </div>
           <div class="icon">
             <i class="ion ion-person-add"></i>
@@ -70,7 +70,7 @@
           <div class="inner">
             <h3>65</h3>
 
-            <p>Unique Visitors</p>
+            <p>Transaksi</p>
           </div>
           <div class="icon">
             <i class="ion ion-pie-graph"></i>
