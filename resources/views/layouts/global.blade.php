06fa896c94b182835f3d1ca2b37d9c24dfdafe37
bayupras141 <aebayu789@gmail.com>
2022-04-05 08:30:29 +0700

    add route tipe

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -109,7 +109,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{route('tipe.index')}}" class="nav-link">
                   <i class="nav-icon fas fa-list"></i>
                   <p>
                     Tipe
