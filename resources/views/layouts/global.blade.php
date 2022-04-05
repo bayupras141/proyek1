59be0a82f6baf6a2b77f83c587140aad24654fa4
bayupras141 <aebayu789@gmail.com>
2022-04-05 10:58:56 +0700

    add route transaksi

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -133,7 +133,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{route('transaksi.index')}}" class="nav-link">
                   <i class="nav-icon fas fa-book"></i>
                   <p>
                     Transaksi
