ac4e34c571fd81c4059d998b3f597777ae17e722
bayupras141 <aebayu789@gmail.com>
2022-04-05 19:21:19 +0700

    extrac coding navbar + sidebar + footer

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -34,118 +34,9 @@
     <img class="animation__shake" src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
   </div>
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-    </ul>
+ @include('layouts.navbar')
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-
-        <!-- Logout Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" v-pre>
-                {{ Auth::user()->name }}
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-             {{ __('Logout') }}
-            </a>
-            
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </div>
-    </li>
-    <!-- END Logout Dropdown Menu -->
-    
-    <!-- Full screen menu -->
-    <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-            <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-    </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="{{route('home')}}" class="brand-link">
-      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Penjualan Motor</span>
-    </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-      
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          
-            <li class="nav-item">
-                <a href="{{route('home')}}" class="nav-link">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <p>
-                    Dashboard
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('tipe.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-list"></i>
-                  <p>
-                    Tipe
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('produk.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-tag"></i>
-                  <p>
-                    Produk
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('customer.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-users"></i>
-                  <p>
-                    Customer
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{route('transaksi.index')}}" class="nav-link">
-                  <i class="nav-icon fas fa-book"></i>
-                  <p>
-                    Transaksi
-                  </p>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="nav-icon fas fa-print"></i>
-                  <p>
-                    Laporan
-                  </p>
-                </a>
-            </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+  @include('layouts.sidebar')
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
@@ -167,13 +58,8 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.2.0
-    </div>
-  </footer>
+
+  @include('layouts.footer')
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
