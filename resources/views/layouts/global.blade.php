626cdcac73aedec1abd2d2f9ec30a5219bf93f86
bayupras141 <aebayu789@gmail.com>
2022-04-05 11:08:01 +0700

    hapus control sidebar

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -71,14 +71,6 @@
             <i class="fas fa-expand-arrows-alt"></i>
         </a>
     </li>
-
-    <!-- Control Sidebar & Change dark mode -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-
     </ul>
   </nav>
   <!-- /.navbar -->
