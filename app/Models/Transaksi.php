5f5a802aec8601b486e76ed4f9ce1fe0bd9035b7
bayupras141 <aebayu789@gmail.com>
2022-04-05 18:56:59 +0700

    model Transaksi

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'type';
+
+    protected $fillable = [
+        'invoice',
+        'total_bayar',
+        'status',
+        'customer_id',
+        'produk_id',
+    ];
+}
