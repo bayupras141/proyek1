6b34a59f3e6056cf57d414ebbea621b7a5884c3b
bayupras141 <aebayu789@gmail.com>
2022-04-05 08:33:41 +0700

    fix typo

--- app/Models/Tipe.php
+++ app/Models/Tipe.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tipe extends Model
 {
     use HasFactory;
-    protected $table = 'tipe';
+    protected $table = 'type';
 
     protected $fillable = [
         'merk',
