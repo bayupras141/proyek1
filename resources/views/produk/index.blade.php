7fd00d7bc86274d5b2546b492fe5783d04b1a7fe
BintangNur <bintangnur46@gmail.com>
2022-04-05 10:08:08 +0700

    update index

--- resources/views/produk/index.blade.php
+++ resources/views/produk/index.blade.php
@@ -10,5 +10,43 @@
     </div><!-- /.container-fluid -->
 @endsection
 @section('content')
-
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Table Produk</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              <div id="example2_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6"></div>
+                    <div class="col-sm-12 col-md-6"></div>
+                </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table id="example2" class="table table-bordered table-hover dataTable dtr-inline" aria-describedby="example2_info">
+                                <thead>
+                                <tr>
+                                    <th class="sorting sorting_asc" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending">Nama</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Warna</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Harga</th>
+                                    <th class="sorting" tabindex="0" aria-controls="example2" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending">Stok</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="odd">
+                                      <td class="dtr-control sorting_1" tabindex="0">Gecko</td>
+                                      <td>Firefox 1.0</td>
+                                      <td>Firefo</td>
+                                      <td>Firef</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+          </div>  
+    </div>
+</div>
 @endsection
