f9f86bf85432e0561df73f993fa52c5b620e0fd1
bayupras141 <aebayu789@gmail.com>
2022-04-05 19:24:55 +0700

    hapus coding tidak penting

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -56,12 +56,6 @@
   <!-- /.content-wrapper -->
 
   @include('layouts.footer')
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
