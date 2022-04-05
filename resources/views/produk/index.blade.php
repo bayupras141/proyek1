a734acb3563a4ce127d56fe0a4519711239a663b
BintangNur <bintangnur46@gmail.com>
2022-04-05 10:03:51 +0700

    layouting produk

---
+++ resources/views/produk/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.global')
+@section('title', 'Produk')
+@section('contentHeader')
+    <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Produk</h1>
+          </div>
+        </div>
+    </div><!-- /.container-fluid -->
+@endsection
+@section('content')
+
+@endsection
