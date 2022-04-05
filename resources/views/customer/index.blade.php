4ae4c53f89216351550b99a7d95620a6818556db
bayupras141 <aebayu789@gmail.com>
2022-04-05 19:09:03 +0700

    add alert

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -7,6 +7,14 @@
             <h1 class="m-0">Customers</h1>
           </div>
         </div>
+        @if (session('status'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('status') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
     </div><!-- /.container-fluid -->
 @endsection
 @section('content')
