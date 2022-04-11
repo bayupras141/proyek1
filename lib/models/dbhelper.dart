c6feeeb83e29f7770c74ff889047580136d67cae
bayupras141 <aebayu789@gmail.com>
2022-04-11 10:37:46 +0700

    create dbHelper

---
+++ lib/models/dbhelper.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:katalog_motor/models/motor.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _databaseHelper;
+  DatabaseHelper._instance() {
+    _databaseHelper = this;
+  }
+
+  factory DatabaseHelper() => _databaseHelper ?? DatabaseHelper._instance();
+
+  static Database? _database;
+
+  Future<Database?> get database async {
+    if (_database == null) {
+      _database = await _initDb();
+    }
+    return _database;
+  }
+
+  static const String _tbl = 'motor';
+
+  Future<Database> _initDb() async {
+    final path = await getDatabasesPath();
+    final databasePath = '$path/motor.db';
+
+    var db = await openDatabase(databasePath, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  void _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE  '$_tbl' (
+        id INTEGER PRIMARY KEY,
+        nama TEXT,
+        merk TEXT,
+        image TEXT,
+        descreption TEXT,
+      );
+    ''');
+  }
+
+}
