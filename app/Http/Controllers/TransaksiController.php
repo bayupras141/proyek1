cc5fc39d66b79ff9b17e415016fedb66c7b144af
TigorImamHamdani <2031710058@student.polinema.ac.id>
2022-06-07 08:04:04 +0700

    transaksi controller

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -2,36 +2,72 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use App\Models\Transaksi;
 
 class TransaksiController extends Controller{
     public function index(){
-        $data = array('title' => 'Data Transaksi');
-        return view ('transaksi.index', $data);
+        return view('transaksi.index', [
+            'transaksi' => Transaksi::all()
+        ]);
     }
 
     public function create(){
         //
+        return view('transaksi.create');
     }
 
     public function store(Request $request){
         //
+        $request->validate([
+            'invoice' => 'required',
+            'total_bayar' => 'required',
+            'diskon' => 'required',
+            'ongkir' => 'required',
+            'status' => 'required',
+            'customer_id' => 'required',
+            'produk_id' => 'required'
+        ]);
+
+        // create new tipe
+        $transaksi = new Transaksi;
+        $transaksi->invoice = $request->invoice;
+        $transaksi->total_bayar = $request->total_bayar;
+        $transaksi->diskon = $request->diskon;
+        $transaksi->ongkir = $request->ongkir;
+        $transaksi->status = $request->status;
+        $transaksi->customer_id = $request->customer_id;
+        $transaksi->produk_id = $request->produk_id;
+        $transaksi->save();
+
+        // return to tipe.index wirt success message
+        return redirect()->route('transaksi.index')->with('status', 'Transaksi berhasil ditambahkan');
     }
 
     public function show($id){
-        $data = array('title' => 'Detail Transaksi');
-        return view('transaksi.show', $data);
+       
     }
 
     public function edit($id){
-        $data = array('title' => 'Form Edit Transaksi');
-        return view('transaksi.edit', $data);
+        return view('transaksi.edit', ['transaksi' => $transaksi]);
     }
 
     public function update(Request $request, $id){
         //
+        $request->validate([
+            'invoice' => 'required',
+            'total_bayar' => 'required',
+            'diskon' => 'required',
+            'ongkir' => 'required',
+            'status' => 'required',
+            'customer_id' => 'required',
+            'produk_id' => 'required'
+        ]);
     }
 
     public function destroy($id){
         //
+        $transaksi->delete();
+        // return to tipe.index wirt success message
+        return redirect()->route('transaksi.index')->with('status', 'Transaksi berhasil dihapus');
     }
 }
