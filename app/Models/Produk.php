08a15005eba138f3c29c9e6495a3cbc6958d41b4
bayupras141 <aebayu789@gmail.com>
2022-06-14 20:43:10 +0700

    first commit

---
+++ app/Models/Produk.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    use HasFactory;
+    protected $table = 'produk';
+
+    protected $fillable = [
+        'nama',
+        'warna',
+        'harga',
+        'stok',
+    ];
+    
+    public function tipe()
+    {
+    	return $this->hasMany(Tipe::class, 'id');
+    }
+}
---
+++ app/Models/Transaksi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+    protected $table = 'transaksi';
+
+    protected $fillable = [
+        'invoice',
+        'total_bayar',
+        'diskon',
+        'ongkir',
+        'status',
+        'customer_id',
+        'produk_id',
+    ];
+}
+}
